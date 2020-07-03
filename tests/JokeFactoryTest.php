18904696916da289a2260b82106d7335fd747fa8
AIMEN.S.A.SASI <aimensasi@icloud.com>
2020-07-03 17:51:54 +0800

    Apply fixes from StyleCI (#1)

--- tests/JokeFactoryTest.php
+++ tests/JokeFactoryTest.php
@@ -5,22 +5,22 @@ namespace Aimensasi\Jokes\Tests;
 use Aimensasi\Jokes\JokeFactory;
 use PHPUnit\Framework\TestCase;
 
-class JokeFactoryTest extends TestCase{
+class JokeFactoryTest extends TestCase
+{
+    /**  @test */
+    public function it_returns_random_joke()
+    {
+        $jokesArr = [
+            'randome joke A',
+            'randome joke B',
+            'randome joke C',
+            'randome joke D',
+            'randome joke E',
+        ];
+        $jokes = new JokeFactory();
 
+        $joke = $jokes->getRandomJoke();
 
-	/**  @test */
-	public function it_returns_random_joke(){
-		$jokesArr = [
-			'randome joke A',
-			'randome joke B',
-			'randome joke C',
-			'randome joke D',
-			'randome joke E',
-		];
-		$jokes = new JokeFactory();
-
-		$joke = $jokes->getRandomJoke();
-
-		$this->assertContains($joke, $jokesArr);
-	}
+        $this->assertContains($joke, $jokesArr);
+    }
 }
