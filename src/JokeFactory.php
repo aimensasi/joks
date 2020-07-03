c58d78363dc4a09a86072dce9feeed020fd0b838
aimensasi <aimensasi@icloud.com>
2020-07-03 16:59:00 +0800

    First Commit

---
+++ src/JokeFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Aimensasi\Jokes;
+
+class JokeFactory {
+
+	protected $jokes = [
+		'randome joke A',
+		'randome joke B',
+		'randome joke C',
+		'randome joke D',
+		'randome joke E',
+	];
+
+	public function __construct(array $jokes = null){
+		$this->jokes = $jokes ?? $this->jokes;
+	}
+
+	public function getRandomJoke(){
+		return $this->jokes[array_rand($this->jokes)];
+	}
+}
---
+++ tests/JokeFactoryTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Aimensasi\Jokes\Tests;
+
+use Aimensasi\Jokes\JokeFactory;
+use PHPUnit\Framework\TestCase;
+
+class JokeFactoryTest extends TestCase{
+
+
+	/**  @test */
+	public function it_returns_random_joke(){
+		$jokesArr = [
+			'randome joke A',
+			'randome joke B',
+			'randome joke C',
+			'randome joke D',
+			'randome joke E',
+		];
+		$jokes = new JokeFactory();
+
+		$joke = $jokes->getRandomJoke();
+
+		$this->assertContains($joke, $jokesArr);
+	}
+}
