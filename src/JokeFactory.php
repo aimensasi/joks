4b56d568253faf986547bfb957accb1dd560239d
aimensasi <aimensasi@icloud.com>
2020-07-03 09:51:46 +0000

    Apply fixes from StyleCI

--- src/JokeFactory.php
+++ src/JokeFactory.php
@@ -2,21 +2,23 @@
 
 namespace Aimensasi\Jokes;
 
-class JokeFactory {
+class JokeFactory
+{
+    protected $jokes = [
+        'randome joke A',
+        'randome joke B',
+        'randome joke C',
+        'randome joke D',
+        'randome joke E',
+    ];
 
-	protected $jokes = [
-		'randome joke A',
-		'randome joke B',
-		'randome joke C',
-		'randome joke D',
-		'randome joke E',
-	];
+    public function __construct(array $jokes = null)
+    {
+        $this->jokes = $jokes ?? $this->jokes;
+    }
 
-	public function __construct(array $jokes = null){
-		$this->jokes = $jokes ?? $this->jokes;
-	}
-
-	public function getRandomJoke(){
-		return $this->jokes[array_rand($this->jokes)];
-	}
+    public function getRandomJoke()
+    {
+        return $this->jokes[array_rand($this->jokes)];
+    }
 }
